a2a8c9cc827dd6828f39d9a3836bd87b09c9f3a1
Trent Oswald <trentoswald@therebelrobot.com>
2014-10-30 14:24:25 -0600

    Added minimalist page from for client

--- gulp/index.js
+++ gulp/index.js
@@ -11,19 +11,12 @@ var _ = require('lodash');
 var minifyCss = require('gulp-minify-css');
 var minifyHtml = require('gulp-minify-html');
 var nodemon = require('gulp-nodemon');
-var clean = require('gulp-rimraf');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 var util = require('gulp-util');
 var yargs = require('yargs');
-
-gulp.task('clean', function() {
-  gulp.src('./server/public', {
-      read: false
-    })
-    .pipe(clean());
-});
 
 gulp.task('browserify', ['jshint-client'], function() {
   return browserify('./client/js/app.js')
@@ -45,8 +38,8 @@ gulp.task('jshint-server', function() {
 
 gulp.task('minifyCSS', function() {
   gulp.src('./server/public/bundle.css')
-    .pipe(minifyCSS())
-    .pipe(source('bundle.min.css'))
+    .pipe(minifyCss())
+    .pipe(rename('bundle.min.css'))
     .pipe(gulp.dest('./server/public/'))
 });
 
@@ -56,10 +49,10 @@ gulp.task('minifyHTML', function() {
     spare: true
   };
   gulp.src(['./client/html/**/*.html', '!./client/html/index.html'])
-    .pipe(minifyHTML(opts))
+    .pipe(minifyHtml(opts))
     .pipe(gulp.dest('./server/public/templates/'))
   gulp.src('./client/html/index.html')
-    .pipe(minifyHTML(opts))
+    .pipe(minifyHtml(opts))
     .pipe(gulp.dest('./server/public/'))
 });
 
@@ -73,80 +66,39 @@ gulp.task('sass', function() {
     .on('error', function(err) {
       console.log(err.message);
     })
-    .pipe(source('bundle.css'))
+    .pipe(rename('bundle.css'))
     .pipe(gulp.dest('./server/public/'));
 });
 
 gulp.task('uglify', ['browserify'], function() {
   gulp.src('./server/public/bundle.js')
     .pipe(uglify())
-    .pipe(source('bundle.min.js'))
+    .pipe(rename('bundle.min.js'))
     .pipe(gulp.dest('./server/public/'))
 
 });
 
 gulp.task('assets', function() {
-  gulp.src(['./client/fonts', './client/img', './client/misc'])
-    .pipe(gulp.dest('./server/public/'))
+  gulp.src('./client/fonts/**/*')
+    .pipe(gulp.dest('./server/public/fonts/'))
+  gulp.src('./client/img/**/*')
+    .pipe(gulp.dest('./server/public/img/'))
+  gulp.src('./client/misc/**/*')
+    .pipe(gulp.dest('./server/public/misc/'))
 });
 
 gulp.task('watch', function() {
-  var watchJSclient = gulp.watch('./client/js/**/*.js', ['jshint-client',
-    'browserify'
+  gulp.watch('client/js/**/*.js', ['jshint-client', 'browserify']);
+  gulp.watch(['server/**/*.js', '!server/public/**'], ['jshint-server']);
+  gulp.watch('client/sass/**/*.scss', ['sass']);
+  gulp.watch('client/html/**/*.html', ['minifyHTML']);
+  gulp.watch(['client/fonts/**/*', 'client/img/**/*', 'client/misc/**/*'], [
+    'assets'
   ]);
-  var watchJSserver = gulp.watch(['./server/**/*.js', '!./server/public/**'], [
-    'jshint-server'
-  ]);
-  var watchCSS = gulp.watch('./client/sass/**/*.scss', ['sass']);
-  var watchHTML = gulp.watch('./client/html/**/*.html', ['minifyHTML']);
-  var watchAssets = gulp.watch(['./client/fonts/**/*', './client/img/**/*',
-    './client/misc/**/*'
-  ], ['assets']);
-  watchJSclient.on('change', function(event) {
-    console.log('File ' + event.path + ' was ' + event.type +
-      ', running tasks...');
-  });
-  watchJSserver.on('change', function(event) {
-    console.log('File ' + event.path + ' was ' + event.type +
-      ', running tasks...');
-  })
-  watchCSS.on('change', function(event) {
-    console.log('File ' + event.path + ' was ' + event.type +
-      ', running tasks...');
-  })
-  watchHTML.on('change', function(event) {
-    console.log('File ' + event.path + ' was ' + event.type +
-      ', running tasks...');
-  })
-  watchAssets.on('change', function(event) {
-    console.log('File ' + event.path + ' was ' + event.type +
-      ', running tasks...');
-  });
 })
 
-gulp.task('nodemon', function() {
-  nodemon({
-      script: 'app.js',
-      ext: 'html js',
-      watch: [
-        "server/"
-      ],
-      ignore: [
-        '**/*.ignore.*',
-        '.git',
-        'node_modules/**/node_modules',
-        'node_modules',
-        'client'
-      ]
-    })
-    .on('restart', function() {
-      console.log('Nodemon restarting...')
-    })
 
-});
-
-gulp.task('default', ['clean', 'jshint-server', 'browserify', 'sass',
-  'minifyHTML'
+gulp.task('default', ['jshint-server', 'browserify', 'sass',
+  'minifyHTML', 'assets', 'watch'
 ]);
 gulp.task('deploy', []);
-gulp.task('develop', ['default', 'watch', 'nodemon']);
--- server/index.js
+++ server/index.js
@@ -1,28 +1,31 @@
-    var bluebird = require('bluebird');
-    var brototype = require('brototype');
-    var browserify = require('browserify');
-    var concat = require('gulp-concat');
-    var debug = require('debug');
-    var exec = require('gulp-exec');
-    var gulp = require('gulp');
-    var gulpif = require('gulp-if');
-    var hapi = require('hapi');
-    var jshint = require('gulp-jshint');
-    var jshintStylish = require('jshint-stylish');
-    var lodash = require('lodash');
-    var minifyCss = require('gulp-minify-css');
-    var minifyHtml = require('gulp-minify-html');
-    var mocha = require('mocha');
-    var moment = require('moment');
-    var nodemon = require('gulp-nodemon');
-    var numeral = require('numeral');
-    var page = require('page');
-    var progress = require('progress');
-    var rimraf = require('gulp-rimraf');
-    var sass = require('gulp-sass');
-    var stringToJs = require('string-to-js');
-    var superagent = require('superagent');
-    var uglify = require('gulp-uglify');
-    var unirest = require('unirest');
-    var util = require('gulp-util');
-    var yargs = require('yargs');
+var debug = require('debug');
+var lodash = require('lodash');
+var progress = require('progress');
+var restify = require('restify');
+var unirest = require('unirest');
+var yargs = require('yargs');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.authorizationParser());
+server.use(restify.dateParser());
+server.use(restify.queryParser());
+server.use(restify.jsonp());
+server.use(restify.gzipResponse());
+server.use(restify.bodyParser());
+
+server.get('/api/:name', respond);
+server.head('/api/:name', respond);
+server.get(/\/?.*/, restify.serveStatic({
+  directory: './server/public',
+  default: 'index.html'
+}));
+server.listen(3210, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
